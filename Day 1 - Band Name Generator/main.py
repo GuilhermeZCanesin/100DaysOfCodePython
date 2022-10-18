70dbd7a5beec59e8e4398bfbe397880a4279f28f
Guilherme Canesin <78756311+GuilhermeZCanesin@users.noreply.github.com>
2022-10-18 02:13:11 +0000

    Day 1 - Complete

---
+++ Day 1 - Band Name Generator/main.py
@@ -0,0 +1,6 @@
+#DAY 1
+
+city_name = input("What´s the name of your city?\n")
+pet_name = input("What´s the name of your pet?\n")
+print("You should call your band: " + city_name + " " + pet_name)
+
