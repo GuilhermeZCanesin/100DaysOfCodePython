6c94fea4ebf2ef5712e04223a118e3c24e6c3f69
Guilherme Canesin <78756311+GuilhermeZCanesin@users.noreply.github.com>
2022-12-19 11:42:44 +0000

    Day 8 - Completed

--- Beginner/Day 8 - Caesar Cipher/main.py
+++ Beginner/Day 8 - Caesar Cipher/main.py
@@ -1,4 +1,4 @@
-#DAY 9
+#DAY 8
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1','2','3','4','5','6','7','8','9',' ']
 
 direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt: ")
